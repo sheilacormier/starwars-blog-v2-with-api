9fc0985dd34d6ab8466fad2f9e136ea87f258993
scormier1499 <sheila_cormier@hotmail.com>
2021-04-10 02:19:53 +0000

    4th commit

--- src/js/component/planetstats.js
+++ src/js/component/planetstats.js
@@ -1,5 +1,40 @@
 import React, { Component } from "react";
 import { Context } from "../store/appContext";
 import { Link, useParams } from "react-router-dom";
+import PropTypes from "prop-types";
 
-export const PlanetStats = () => <div className="container mt-5 mb-5">PlanetStats test</div>;
+export const PlanetStats = props => {
+	const [planets, setPlanets] = useState(undefined);
+	useEffect(() => {
+		fetch(`https://www.swapi.tech/api/planets/${props.data.uid}`)
+			.then(res => res.json())
+			.then(data => setPlanets(data.result.properties));
+	}, [props.data]);
+
+	return (
+		<div className="container mt-5 mb-5">
+			<div className="row mb-3 mt-4">
+				<div className="col text-center detailsheader">DIAMETER</div>
+				<div className="col text-center detailsheader">ROTATION PERIOD</div>
+				<div className="col text-center detailsheader">GRAVITY</div>
+				<div className="col text-center detailsheader">POPULATION</div>
+				<div className="col text-center detailsheader">CLIMATE</div>
+				<div className="col text-center detailsheader">TERRAIN</div>
+			</div>
+			{typeof person !== "undefined" && (
+				<div className="row row mb-5 detailspadding">
+					<div className="col text-center detailsattributes">{planets.diameter}</div>
+					<div className="col text-center detailsattributes">{planets.rotation_period}</div>
+					<div className="col text-center detailsattributes">{planets.gravity}</div>
+					<div className="col text-center detailsattributes">{planets.population}</div>
+					<div className="col text-center detailsattributes">{planets.climate}</div>
+					<div className="col text-center detailsattributes">{planets.terrain}</div>
+				</div>
+			)}
+		</div>
+	);
+};
+
+PlanetStats.propTypes = {
+	data: PropTypes.object.isRequired
+};
--- src/js/component/starshipstats.js
+++ src/js/component/starshipstats.js
@@ -1,5 +1,40 @@
 import React, { Component } from "react";
 import { Context } from "../store/appContext";
 import { Link, useParams } from "react-router-dom";
+import PropTypes from "prop-types";
 
-export const StarshipStats = () => <div className="container mt-5 mb-5">StarshipStats test</div>;
+export const StarshipStats = props => {
+	const [starships, setStarships] = useState(undefined);
+	useEffect(() => {
+		fetch(`https://www.swapi.tech/api/starships/${props.data.uid}`)
+			.then(res => res.json())
+			.then(data => setStarship(data.result.properties));
+	}, [props.data]);
+
+	return (
+		<div className="container mt-5 mb-5">
+			<div className="row mb-3 mt-4">
+				<div className="col text-center detailsheader">MODEL</div>
+				<div className="col text-center detailsheader">STARSHIP CLASS</div>
+				<div className="col text-center detailsheader">LENGHT</div>
+				<div className="col text-center detailsheader">CREW</div>
+				<div className="col text-center detailsheader">PASSENGERS</div>
+				<div className="col text-center detailsheader">MGLT</div>
+			</div>
+			{typeof person !== "undefined" && (
+				<div className="row row mb-5 detailspadding">
+					<div className="col text-center detailsattributes">{starships.model}</div>
+					<div className="col text-center detailsattributes">{starships.starship_class}</div>
+					<div className="col text-center detailsattributes">{starships.length}</div>
+					<div className="col text-center detailsattributes">{starships.crew}</div>
+					<div className="col text-center detailsattributes">{starships.passengers}</div>
+					<div className="col text-center detailsattributes">{starships.MGLT}</div>
+				</div>
+			)}
+		</div>
+	);
+};
+
+StarshipStats.propTypes = {
+	data: PropTypes.object.isRequired
+};
