ad0d9ac9b96cb7dec8d296c26efc2e96372a44f1
sheilacormier <sheila_cormier@hotmail.com>
2021-04-25 13:49:40 +0000

    added description from api

--- src/js/views/people.js
+++ src/js/views/people.js
@@ -39,13 +39,7 @@ export const People = props => {
 							</div>
 							<div className="col-12 col-lg-5 p-3 p-xl-4 detailsdescription detailsborder">
 								<h3 className="detailsname">{store[type][currentID].name}</h3>
-								<p>
-									Simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-									industrys standard dummy text ever since the 1500s, when an unknown printer took a
-									galley of type and scrambled it to make a type specimen book. It has survived not
-									only five centuries, but also the leap into electronic typesetting, remaining
-									essentially unchanged.
-								</p>
+								<p>{store[type][currentID].description}</p>
 								<Link to="/">
 									<button className="btn btn-warning">Back home</button>
 								</Link>
