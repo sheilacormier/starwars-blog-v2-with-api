0f1e8ea5a47506524b412718267910ac1ccbf43a
sheilacormier <sheila_cormier@hotmail.com>
2021-05-01 11:19:13 +0000

    updated footer 2

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 
+
 export const Footer = () => (
 	<footer className="footer mt-auto py-5 text-center footerbg">
 		<p className="copyright">© Lucasfilm Ltd. All Rights Reserved | Powered by Sheila Cormier</p>
